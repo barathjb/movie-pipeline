72af21c2c3f3cf48a4657c9e9888680da3af9cec
barathjb <cbjbarath@gmail.com>
2024-03-18 08:07:12 +0000

    updating push.sh

--- push.sh
+++ push.sh
@@ -1,4 +1,7 @@
-git status
+echo "############### running git add ###################"
 git add .
+echo "############### running git commit ###################"
 git commit -m "$1"
+
+echo "############### running git push ###################"
 git push origin main
