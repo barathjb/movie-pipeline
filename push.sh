903e652ccca008ff3d69b35fe73291fab1baba63
barathjb <cbjbarath@gmail.com>
2024-03-18 10:01:23 +0000

    update yaml

--- push.sh
+++ push.sh
@@ -1,3 +1,5 @@
+git config --global user.email "cbjbarath@gmail.com"
+git config --global user.name "barathjb"
 echo "############### running git add ###################"
 git add .
 echo "############### running git commit ###################"
