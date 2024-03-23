79e02f69c8a85160dd177d21da1b76b4d86654e0
barathjb <cbjbarath@gmail.com>
2024-03-23 23:12:46 +0530

    Delete push.sh

--- push.sh
+++
@@ -1,9 +0,0 @@
-git config --global user.email "cbjbarath@gmail.com"
-git config --global user.name "barathjb"
-echo "############### running git add ###################"
-git add .
-echo "############### running git commit ###################"
-git commit -m "$1"
-
-echo "############### running git push ###################"
-git push origin main
